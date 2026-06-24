64592665e4b0ec12ee3f88525a4358e9fc29e566
mianxabdullah <mian.abdullah.ayub@gmail.com>
2026-06-24 14:33:19 +0500

    added payments,deliveries and reviews tables in DB

--- create_tables.sql
+++ create_tables.sql
@@ -81,3 +81,100 @@ CREATE TABLE order_details (
         REFERENCES menu_items(item_id)
 );
 
+-- PAYMENTS
+-- One payment per order (1:1)
+CREATE TABLE payments (
+    payment_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
+    order_id INT UNIQUE NOT NULL,
+
+    payment_method VARCHAR(20)
+    CHECK (
+        payment_method IN (
+            'Cash',
+            'Credit Card',
+            'Debit Card',
+            'Easypaisa',
+            'JazzCash'
+        )
+    ),
+
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    amount NUMERIC(10,2)
+    CHECK (amount >= 0),
+
+    CONSTRAINT fk_payment_order
+        FOREIGN KEY (order_id)
+        REFERENCES orders(order_id)
+        ON DELETE CASCADE
+);
+
+-- DELIVERIES
+-- One delivery per order (1:1)
+CREATE TABLE deliveries (
+    delivery_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
+    order_id INT UNIQUE NOT NULL,
+    agent_id INT NOT NULL,
+
+    delivery_status VARCHAR(20)
+    CHECK (
+        delivery_status IN (
+            'Assigned',
+            'Picked Up',
+            'Out for Delivery',
+            'Delivered'
+        )
+    ),
+
+    delivery_time TIMESTAMP,
+
+    CONSTRAINT fk_delivery_order
+        FOREIGN KEY (order_id)
+        REFERENCES orders(order_id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_delivery_agent
+        FOREIGN KEY (agent_id)
+        REFERENCES delivery_agents(agent_id)
+);
+
+CREATE TABLE reviews (
+    review_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
+    customer_id INT NOT NULL,
+
+    restaurant_id INT NULL,
+    agent_id INT NULL,
+    order_id INT NULL,
+
+    rating INT CHECK (rating BETWEEN 1 AND 5),
+
+    comment TEXT,
+
+    CONSTRAINT fk_reviews_customer
+        FOREIGN KEY (customer_id)
+        REFERENCES customer(customer_id),
+
+    CONSTRAINT fk_reviews_restaurant
+        FOREIGN KEY (restaurant_id)
+        REFERENCES restaurant(restaurant_id),
+
+    CONSTRAINT fk_reviews_agent
+        FOREIGN KEY (agent_id)
+        REFERENCES delivery_agents(agent_id),
+
+    CONSTRAINT fk_reviews_order
+        FOREIGN KEY (order_id)
+        REFERENCES orders(order_id),
+
+    -- ensures review is for ONLY ONE target
+    CONSTRAINT chk_review_target
+    CHECK (
+        (restaurant_id IS NOT NULL)::int +
+        (agent_id IS NOT NULL)::int +
+        (order_id IS NOT NULL)::int = 1
+    )
+);
+
